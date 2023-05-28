deaaf7a1d240391914aaec8bdbbc6df915c372de
poncho_boncho <Sorkolas@mail.ru>
2023-05-28 20:42:24 +0300

    added MainActivity.kt

---
+++ app/src/main/java/poncho/boncho/studentsdiary/MainActivity.kt
@@ -0,0 +1,11 @@
+package poncho.boncho.studentsdiary
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
